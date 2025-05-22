1d6658f4de8f3eaf9e370c31178b4dfc40128953
Maxim Kloster <59746763+Maxknax@users.noreply.github.com>
2025-05-22 09:19:24 +0000

    trying sth

--- server.py
+++ server.py
@@ -3,8 +3,21 @@ from flask import Flask, request, jsonify
 import json
 import os
 
+
+
 app = Flask(__name__)
 SCORE_FILE = "highscores.json"
+
+from flask_cors import CORS
+CORS(app)
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 # Highscores laden
 def load_scores():
